18c60a67d8ed40ea318a25290af7d2e11fe6e5c0
Rana Emad <rana3emad@gmail.com>
2020-05-27 20:46:03 +0200

    initial build

---
+++ precache-manifest.f66e3d38ddec420bb4667cb3cbf6c8ed.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856a5704e6551af5e03d17bfd6ef96a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04116b0681d4f08da1d9",
+    "url": "/static/css/main.31110a5f.chunk.css"
+  },
+  {
+    "revision": "adeb939404d76a960e0b",
+    "url": "/static/js/2.43114864.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.43114864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04116b0681d4f08da1d9",
+    "url": "/static/js/main.2256fd78.chunk.js"
+  },
+  {
+    "revision": "1dd248866766d22423c7",
+    "url": "/static/js/runtime-main.70e77f7a.js"
+  },
+  {
+    "revision": "664aa5d7a9d3f8820616a6e17e768d32",
+    "url": "/static/media/friday.664aa5d7.svg"
+  },
+  {
+    "revision": "6af1656621e0c44624f50c7167eaf416",
+    "url": "/static/media/monday.6af16566.svg"
+  },
+  {
+    "revision": "64d0c4b5a244b566361cb650f12f6e2c",
+    "url": "/static/media/saturday.64d0c4b5.svg"
+  },
+  {
+    "revision": "0256b07845db59084042c687e78e76f3",
+    "url": "/static/media/sunday.0256b078.svg"
+  },
+  {
+    "revision": "76a23e166e997b60637235f1cb1d686e",
+    "url": "/static/media/thursday.76a23e16.svg"
+  },
+  {
+    "revision": "c5117fae1f7070d7294b1c1dcba76ac1",
+    "url": "/static/media/tuesday.c5117fae.svg"
+  },
+  {
+    "revision": "686bfb6b757c5dc8102b7d65fe5f6163",
+    "url": "/static/media/wednesday.686bfb6b.svg"
+  }
+]);
