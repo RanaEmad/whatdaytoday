0ccc41aa2fe986b5deb4a023b0f2f2bb5f386196
Rana Emad <rana3emad@gmail.com>
2020-05-27 21:02:39 +0200

    second build

---
+++ precache-manifest.2d259653cd78e3f22a62b3238ed5e5dc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cf1aef4cf445db8629d1d49a068860f",
+    "url": "/whatdaytoday/index.html"
+  },
+  {
+    "revision": "03ef8123d0a4f424ac6e",
+    "url": "/whatdaytoday/static/css/main.31110a5f.chunk.css"
+  },
+  {
+    "revision": "adeb939404d76a960e0b",
+    "url": "/whatdaytoday/static/js/2.43114864.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whatdaytoday/static/js/2.43114864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ef8123d0a4f424ac6e",
+    "url": "/whatdaytoday/static/js/main.5d7a240b.chunk.js"
+  },
+  {
+    "revision": "93266760d4e4e8a701eb",
+    "url": "/whatdaytoday/static/js/runtime-main.1b795d37.js"
+  },
+  {
+    "revision": "664aa5d7a9d3f8820616a6e17e768d32",
+    "url": "/whatdaytoday/static/media/friday.664aa5d7.svg"
+  },
+  {
+    "revision": "6af1656621e0c44624f50c7167eaf416",
+    "url": "/whatdaytoday/static/media/monday.6af16566.svg"
+  },
+  {
+    "revision": "64d0c4b5a244b566361cb650f12f6e2c",
+    "url": "/whatdaytoday/static/media/saturday.64d0c4b5.svg"
+  },
+  {
+    "revision": "0256b07845db59084042c687e78e76f3",
+    "url": "/whatdaytoday/static/media/sunday.0256b078.svg"
+  },
+  {
+    "revision": "76a23e166e997b60637235f1cb1d686e",
+    "url": "/whatdaytoday/static/media/thursday.76a23e16.svg"
+  },
+  {
+    "revision": "c5117fae1f7070d7294b1c1dcba76ac1",
+    "url": "/whatdaytoday/static/media/tuesday.c5117fae.svg"
+  },
+  {
+    "revision": "686bfb6b757c5dc8102b7d65fe5f6163",
+    "url": "/whatdaytoday/static/media/wednesday.686bfb6b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f66e3d38ddec420bb4667cb3cbf6c8ed.js"
+  "/whatdaytoday/precache-manifest.2d259653cd78e3f22a62b3238ed5e5dc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/whatdaytoday/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
